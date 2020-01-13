f2e06f43c2b5b0d48e4e7bf2a86bfcfee9f1a863
Dmitry Nikolaev <mogikanlol@gmail.com>
2020-01-13 20:37:49 +0300

    Improve navbar home button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,13 @@
     <nav>
         <v-app-bar flat>
             <router-link to="/">
-                <v-toolbar-title>
-                    <span>The Header</span>
-                </v-toolbar-title>
+                <v-btn depressed>
+                    <span>The Header Home</span>
+                </v-btn>
             </router-link>
             <v-spacer></v-spacer>
             <v-btn depressed>
-                <span>Sign Out</span>
+                <span>Sign In</span>
             </v-btn>
         </v-app-bar>    
     </nav>
@@ -20,3 +20,9 @@ export default {
     
 }
 </script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>
