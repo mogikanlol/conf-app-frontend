59de181997d6f024e80380d189084aa5c8f44e22
Dmitry Nikolaev <mogikanlol@gmail.com>
2020-01-21 23:25:28 +0300

    Remove comment

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,8 @@
   <v-app >
     <v-content class="grey lighten-4" >
       <Navbar />
-      <!-- <v-container> -->
         <router-view></router-view>
-      <!-- </v-container> -->
     </v-content>
-    
   </v-app>
 </template>
 
